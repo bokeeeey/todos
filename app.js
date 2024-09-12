d2df452ef2e281ad8af73d03b8456f0beb43eec6
김보경 <rlaqhrud6263@naver.com>
2024-09-13 08:06:24 +0900

    Feat: task id에 따른 다이나믹 라우트 설정 및 예외처리

--- app.js
+++ app.js
@@ -28,6 +28,14 @@ app.get("/tasks", (req, res) => {
   res.send(newTasks);
 });
 
+app.get("/tasks/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const task = tasks.find((task) => task.id === id);
+
+  if (task) res.send(task);
+  else res.status(404).send({ message: "Cannot find given id. " });
+});
+
 app.listen(app.get("port"), () => {
   console.log(app.get("port"), "Server Started!");
 });
