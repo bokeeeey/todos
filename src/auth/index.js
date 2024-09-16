cb7ae427549c4a7ecdeea791db2082fc8b354cb3
김보경 <rlaqhrud6263@naver.com>
2024-09-16 21:32:28 +0900

    Feat: authrozation 관련 부분 테스트 추가

--- src/auth/index.js
+++ src/auth/index.js
@@ -21,3 +21,36 @@ app.patch("/me", validatePassword, (req, res) => {
 app.listen(app.get("port"), () => {
   console.log("Server is lintening on port 3000");
 });
+
+// User router
+const userRouter = express.Router();
+
+userRouter
+  .route("/")
+  .get((req, res, next) => {
+    res.send({ message: "사용자 목록 보기" });
+  })
+  .post((req, res, next) => {
+    res.send({ message: "사용자 추가하기" });
+  });
+
+userRouter
+  .route("/:id")
+  .patch((req, res, next) => {
+    res.send({ message: "사용자 수정하기" });
+  })
+  .delete((req, res, next) => {
+    res.send({ message: "사용자 삭제하기" });
+  });
+
+function adminOnly(req, res, next) {
+  const authorization = req.get("Authorization");
+
+  if (authorization) {
+    next();
+  } else {
+    res.status(401).send({ message: "권한이 없습니다" });
+  }
+}
+
+app.use("/users", adminOnly, userRouter);
